e54a15e3504e7a54df942043b4464d3f9cbe3d93
dev-anonto910 <anontokumar910@gmail.com>
2021-03-15 15:09:14 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+$(function(){
+
+/*============Preloader==================*/
+
+$(window).on('load', function(){
+    $('.preloader').delay(500).fadeOut(500);
+});
+
+//typerd js============
+$(".typed").typed({
+    strings: ["D4X300R.", "D4X300R.", "D4X300R.", "D4X300R."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 5,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+});
+
+
+
+
+});
